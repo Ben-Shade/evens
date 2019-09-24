4e97da9c5d7563b86917faa0a5486fd34768533b
Ben-Shade <55599909+Ben-Shade@users.noreply.github.com>
2019-09-24 14:44:11 -0500

    Add files via upload

---
+++ evens.py
@@ -0,0 +1,10 @@
+#ben shade
+#Evens this program takes your number and tells even numbers from 0 to it.
+
+varible = int(input("please enter a number from 0 to 20 "))
+counter = 0
+while varible >= counter:
+    print(counter , end = " " )
+    counter += 2
+
+input("\n\nPress enter to exit")
